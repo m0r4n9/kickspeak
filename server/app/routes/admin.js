868e518ea3fae3e83eebe7a2770c8ae826282590
sashasevostyanov <sevostanovsasa7@gmail.com>
2023-11-14 22:22:31 +0300

    search route admin panel

--- server/app/routes/admin.js
+++ server/app/routes/admin.js
@@ -9,6 +9,7 @@ const upload = require('../middlewares/uploadImg');
 // Brands
 adminRouter.get('/brands', BrandAdminController.getBrands);
 adminRouter.get('/brand/:id', BrandAdminController.getBrandDetails);
+adminRouter.get('/brands/search', BrandAdminController.searchBrands);
 adminRouter.put('/brand/update/:id', BrandAdminController.updateBrand);
 adminRouter.post('/brand/create', upload.single("logo"), BrandAdminController.createBrand);
 adminRouter.delete('/brand/delete/:id', BrandAdminController.deleteBrand);
